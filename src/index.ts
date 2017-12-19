5e94f8171d00559f6ed5503091a30ad8c064bc52
Mike Smart <mike.smart@jadu.co.uk>
2017-12-19 13:00:03 +0000

    basic engine setup with the PIXI.js renderer

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-alert('Hello, world!');
+import { Application } from 'pixi.js';
+
+// Setup application
+const root: HTMLElement = document.getElementById('root');
+const application: Application = new Application({
+    width: window.innerWidth,
+    height: window.innerHeight
+});
+
+// Append a PIXI canvas to the root element
+root.appendChild(application.view);
+
+// Make our renderer responsive
+application.renderer.autoResize = true;
+
